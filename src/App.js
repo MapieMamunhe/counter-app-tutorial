b8291e0ed21ce33fd1f9dc935f15e2a5af8f0232
Mapie Victorino Mamunhe <mmamunhe@gmail.com>
2022-02-03 00:19:29 +0200

    Navbar Counter
    Com isso aprendi a trabalhar com multiplos componentes em simultaneo usando diferentes propriedades entre componentes de mesmo nivel jogando a responsabilidade para o componente directamente acima deles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+  handleReset = (counterId) => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter }; //Sem isso acontece aquilo do JAva com as referencias
+    counters[index].value++;
+    this.setState({ counters: counters });
+  };
+  render() {
+    return (
+      <>
+        <NavBar
+          totalCounters={
+            this.state.counters.filter((counter) => counter.value > 0).length
+          }
+        />
+        <main className="container">
+          <Counters
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counters={this.state.counters}
+          />
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;

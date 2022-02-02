5fce811cf8737a5720cb72a646de682799c882e8
Mapie Victorino Mamunhe <mmamunhe@gmail.com>
2022-02-03 00:29:50 +0200

    Arguments Destructuring
    Usamos o object destructuring para eliminar a necessidade de escrever this.props

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,19 +2,17 @@ import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
   render() {
+    const { onIncrement, onDelete, onReset, counters } = this.props;
     return (
       <div>
-        <button
-          onClick={() => this.props.onReset()}
-          className="btn btn-primary m-2"
-        >
+        <button onClick={() => onReset()} className="btn btn-primary m-2">
           Reset
         </button>
-        {this.props.counters.map((counter) => (
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-          <span className="badge bg-pill bg-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
-
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge bg-pill bg-secondary">{totalCounters}</span>
+      </a>
+    </nav>
+  );
+};
 export default NavBar;
